914893b9147381065d7e64f0497b1c5acc2c9726
GodaProjects <goda.projects@gmail.com>
2022-09-14 14:32:15 +0530

    Added basic msal stuff, created an object for MSAL and passed it to App.js since if components need the MSAL context, the component needs to be wrapped within the this.

--- src/index.js
+++ src/index.js
@@ -8,12 +8,23 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from './App';
 
+// My changes
+import {PublicClientApplication} from '@azure/msal-browser'
+
+const pca = new PublicClientApplication({
+    auth: {
+        clientId: '2b53c05e-ea15-4229-951e-8e968bf28b9d',
+        authority: 'https://login.microsoftonline.com/05a9b8a4-ccef-4246-9ff6-234ef22c349c/oauth2/v2.0/authorize',
+        redirectUri: '/' 
+    }
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
             <ThemeProvider theme={theme}>
-                <App />
+                <App msalInstance={pca}/>
             </ThemeProvider>
         </BrowserRouter>
     </React.StrictMode>
